f98c95f53dfa8cdb08f9ed04bc1c746ac9f39f1e
Дастан Абдраим <dastan.abdraym@gmail.com>
2020-07-30 14:57:52 +0600

    staticresourceconfig rename

--- server/src/main/java/com/dastan/abdraym/student/config/StaticResourceConfig.java
+++ server/src/main/java/com/dastan/abdraym/student/config/StaticResourceConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class StaticResourceConfiguration implements WebMvcConfigurer {
+public class StaticResourceConfig implements WebMvcConfigurer {
     @Value("${upload.path}")
     private String uploadPath;
 
